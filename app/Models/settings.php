a0aa2ceeb692b1937dca21175588deb082a019b3
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-02-14 11:37:39 +0100

    initialize project structure with basic files and configurations

---
+++ app/Models/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class settings extends Model
+{
+    protected $fillable = [
+        'navItems', 'userId',
+    ];
+
+    
+}
